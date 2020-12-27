1ddf611be5581136ddc0335a6460a683a8047d94
satan021 <70333424+satan021@users.noreply.github.com>
2020-12-28 01:51:09 +0300

    Add files via upload

---
+++ Untitled.java
@@ -0,0 +1,11 @@
+public class Untitled 
+{
+
+    public static void main(String[] args) 
+    {
+        //myInterface2.sum();
+        //myInterface.rere();
+        System.out.println("test");
+    }
+
+}  
